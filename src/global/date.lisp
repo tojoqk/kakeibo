e2246c500e386de2ea8b7db4e71f9d4b88e0d348
Masaya Tojo <masaya@tojo.tokyo>
2023-06-21 22:21:24 +0900

    Use time package in date package

--- src/global/date.lisp
+++ src/global/date.lisp
@@ -4,7 +4,8 @@
   (:shadow #:error)
   (:local-nicknames
    (#:integral #:coalton-library/math/integral)
-   (#:exception #:kakeibo/global/exception))
+   (#:exception #:kakeibo/global/exception)
+   (#:time #:kakeibo/global/time))
   (:export
    #:Date
    #:make
@@ -27,9 +28,9 @@
                  -> Result Error Date))
   (define (make y m d)
     (let date = (%Date y m d))
-    (if (valid? date)
-        (Ok date)
-        (Err InvalidDate)))
+    (match (time:make y m d 0 0 0)
+      ((Ok _) (Ok date))
+      ((Err _) (Err InvalidDate))))
 
   (define-type Error (InvalidDate))
   (exception:define-exception-instance Error)
@@ -42,29 +43,4 @@
     (define (== (%Date y1 m1 d1) (%Date y2 m2 d2))
       (and (== y1 y2)
            (== m1 m2)
-           (== d1 d2))))
-
-  (define (leap? y)
-    (and (== (integral:mod y 4) 0)
-         (not (and (== (integral:mod y 100) 0)
-                   (/= (integral:mod y 400) 0)))))
-
-  (define (valid? (%Date y m d))
-    (and (<= 0 y)
-         (<= 1 d)
-         (match m
-           (1  (<= d 31))
-           (2  (if (leap? y)
-                   (<= d 29)
-                   (<= d 28)))
-           (3  (<= d 31))
-           (4  (<= d 30))
-           (5  (<= d 31))
-           (6  (<= d 30))
-           (7  (<= d 31))
-           (8  (<= d 31))
-           (9  (<= d 30))
-           (10 (<= d 31))
-           (11 (<= d 30))
-           (12 (<= d 31))
-           (_ False)))))
+           (== d1 d2)))))
