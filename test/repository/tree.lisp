fd1d874c8a37a4c762dbb746910c54a76aaf74d3
Masaya Tojo <masaya@tojo.tokyo>
2023-06-25 16:58:51 +0900

    Add Tree Repository's tests

---
+++ test/repository/tree.lisp
@@ -0,0 +1,46 @@
+(defpackage #:kakeibo/test/repository/tree
+  (:use #:coalton-testing)
+  (:local-nicknames
+   (#:tree #:kakeibo/repository/tree)
+   (#:st #:coalton-library/monad/state)
+   (#:entity #:kakeibo/test/entity/repository)))
+
+(in-package #:kakeibo/test/repository/tree)
+
+(coalton-fiasco-init #:kakeibo-test-fiasco)
+
+(coalton-toplevel
+  (define (run test)
+    (match (st:run test
+                   tree:init)
+      ((Tuple _ b) b))))
+
+(cl:defmacro add-test (symbol)
+  `(define-test
+       ,(cl:intern
+         (cl:concatenate 'cl:string
+                         "KAKEIBO/REPOSITORY/TREE-"
+                         (cl:symbol-name symbol)))
+       ()
+     (is (run (,symbol)))))
+
+(add-test entity:test-transaction-create)
+(add-test entity:test-transaction-read)
+(add-test entity:test-transaction-update)
+(add-test entity:test-transaction-delete)
+(add-test entity:test-transaction-different-ids)
+(add-test entity:test-transaction-NotFoundOnRead-error)
+(add-test entity:test-transaction-NotFoundOnUpdate-error)
+(add-test entity:test-transaction-NotFoundOnDelete-error)
+(add-test entity:test-transaction-AssociatedItemsExist-error)
+
+(add-test entity:test-item-create)
+(add-test entity:test-item-read)
+(add-test entity:test-item-update)
+(add-test entity:test-item-delete)
+(add-test entity:test-item-different-ids)
+(add-test entity:test-item-TransactionNotFoundOnCreate-error)
+(add-test entity:test-item-NotFoundOnRead-error)
+(add-test entity:test-item-NotFoundOnUpdate-error)
+(add-test entity:test-item-TransactionNotFoundOnUpdate-error)
+(add-test entity:test-item-NotFoundOnDelete-error)
