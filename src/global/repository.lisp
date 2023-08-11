7c8de91a38dfb072e6a485910c8c5c539fd96031
Masaya Tojo <masaya@tojo.tokyo>
2023-08-11 18:11:50 +0900

    Modify Repository typeclass's interface

--- src/global/repository.lisp
+++ src/global/repository.lisp
@@ -1,10 +1,14 @@
 (cl:defpackage #:kakeibo/global/repository
   (:use #:coalton
         #:coalton-prelude)
-  (:shadow #:error)
-  (:export #:Repository))
+  (:export #:Repository #:connected?
+           #:Config #:connect!))
 
 (cl:in-package #:kakeibo/global/repository)
 
 (coalton-toplevel
-  (define-class (Repository :r)))
+  (define-class (Repository :r)
+    (connected? (:r -> Boolean)))
+
+  (define-class (Repository :r => Config :r :c (:r -> :c))
+    (connect! (:c -> :r))))
